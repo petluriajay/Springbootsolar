c984a4055e62322a7c9efb301553090893740bf3
petluriajay <petluriajay@gmail.com>
2015-10-01 10:55:38 +0530

    changes
    Changes

--- src/main/java/com/example/Example.java
+++ src/main/java/com/example/Example.java
@@ -11,7 +11,7 @@ public class Example {
 
 	@RequestMapping(value = "/")
     String home() {
-        return "Hello World!";
+        return "Hello World Testing!";
     }
 
     public static void main(String[] args) throws Exception {
